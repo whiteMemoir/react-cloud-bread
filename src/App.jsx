ef151ccd724b07a5a98a208eb3c02186997a4a96
namikazepian <namikazepian@gmail.com>
2022-06-27 07:22:06 +0700

    styling + hardcode

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/semantic/Footer";
+import Main from "../components/semantic/Main";
+import Header from "../components/semantic/Header";
+
+const Home = () => {
+	return (
+		// This example requires Tailwind CSS v2.0+
+		<>
+			<Header />
+			<Main />
+			<Footer />
+		</>
+	);
+};
+
+export default Home;

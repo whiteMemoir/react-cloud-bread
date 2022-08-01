9228d1bbb3de570ec8f0cadfbaf1d891b4735264
namikazepian <namikazepian@gmail.com>
2022-08-01 18:06:02 +0700

    sorry lali teros

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-	api_host: "http://localhost:3003",
+	// api_host: "http://localhost:3003",
+	api_host: "https://cloud-bread-server.herokuapp.com/",
 	api_region: "https://www.emsifa.com/api-wilayah-indonesia/api",
 };
 export default config;
